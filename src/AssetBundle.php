71eefb335279143a693bd1cbf589629577d1a7b5
andrey <6097033725>
2021-05-11 18:57:41 +0300

    first commit

---
+++ src/AssetBundle.php
@@ -0,0 +1,15 @@
+<?php
+namespace andrewdanilov\fontawesome;
+
+/**
+ * Class AssetBundle
+ */
+class AssetBundle extends \yii\web\AssetBundle
+{
+    /**
+     * @inherit
+     */
+    public $css = [
+        'https://use.fontawesome.com/releases/v5.15.1/css/all.css',
+    ];
+}
